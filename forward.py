6d2ea531783cc720cfb8f93ace218507ea54bfd9
nu <yuuta_hiramatsu0227@yahoo.co.jp>
2023-12-23 16:16:29 +0900

    stop exist while 0830-1630

--- forward.py
+++ forward.py
@@ -6,18 +6,17 @@ import pandas as pd
 import sys
 
 import mylib_stock2
-from mylib_stock2 import NOW, NOW_TIME, TODAY_LAGGED, START, END
+from mylib_stock2 import NOW_WEEK, NOW_TIME, START, END
 
 
-# 株価の予測結果を検証(フォワードテスト)するプログラム
+# 株価を予測した銘柄の1営業日後の株価をcsvに追記するプログラム
 
 # 各種設定
 OUTPUT_PATH = "datas/output"
 
-
-# 営業時間+-マージンの時間は実行できない
-if (START < NOW_TIME) and (NOW_TIME < END):
-    print("8:30~16:30は実行できません．")
+# 平日は営業時間+-マージンの時間は実行できない
+if (START <= NOW_TIME <= END) and (0 <= NOW_WEEK <= 4):
+    print("平日の8:30~16:30は実行できません．")
     sys.exit()
 
 # 予測結果のファイル一覧を取得
@@ -34,8 +33,8 @@ for filename in filenames:
     # データの追加
     df_file = mylib_stock2.add_price(df_file)
 
-    # 検証用リストへの追加とcsvでの上書き保存
+    # 上書き保存
     if not df_file is None:
-        df_file.to_csv(f"{OUTPUT_PATH}/{filename}", index=False)
-        
+        df_file.to_csv(file_path, index=False)
+
 print("終了しました．")
